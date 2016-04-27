68b83b634f9ed866271fd69fc025084034576046
Dan Siwiec <daniel.siwiec@gmail.com>
2016-04-27 00:14:41 -0700

    Add mongoose

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 'use strict'
 
-let express = require('express')
-let app = express()
+let express = require('express'),
+  app = express(),
+  mongoose = require('mongoose'),
+  bodyParser = require('body-parser')
 
 app.set('port', (process.env.PORT || 5000))
 
+app.use(bodyParser.json())
 app.use(express.static(__dirname + '/public'))
+mongoose.connect('mongodb://waypoints:waypoints@ds021671.mlab.com:21671/heroku_27wrdm9m')
 
-app.listen(app.get('port'), function() {
+
+app.post('/', function (req, res) {
+  let location = req.body
+  var Loc = mongoose.model('locations', {
+    hash: String,
+    name: String,
+    geo: {
+      lat: Number,
+      long: Number
+    }
+  });
+  location.hash = 'abc'
+
+  var record = new Loc(location);
+  record.save(function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('meow');
+    }
+  });
+  res.send(location.hash);
+});
+
+app.get('/:hash', function (req, res) {
+  console.log('you asked for ' + req.params.hash)
+})
+
+app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
