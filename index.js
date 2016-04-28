cce33d4691b938549a455ad26e2b84eb39ff3a1e
Dan Siwiec <daniel.siwiec@gmail.com>
2016-04-27 21:18:52 -0700

    Add sections

--- index.js
+++ index.js
@@ -4,47 +4,39 @@ let express = require('express'),
   app = express(),
   mongoose = require('mongoose'),
   bodyParser = require('body-parser'),
-  shortid = require('shortid')
+  shortid = require('shortid'),
+  Location
 
 app.set('port', (process.env.PORT || 5000))
 
 app.use(bodyParser.json())
 app.use(express.static(__dirname + '/public'))
-mongoose.connect('mongodb://waypoints:waypoints@ds021671.mlab.com:21671/heroku_27wrdm9m')
 
-let Location = mongoose.model('locations', {
-  hash: String,
-  name: String,
-  geo: {
-    lat: Number,
-    long: Number
-  },
-  _id: {
-    type: String,
-    'default': shortid.generate
-  }
-})
+connectToMongo()
 
-app.post('/', function (req, res) {
+app.post('/locations', function (req, res) {
+  console.log(`Saving record ${JSON.stringify(req.body)}`)
   let record = new Location(req.body)
   record.save(function (err) {
     if (err) {
       console.log(`Error when saving ${record._id}: ${err}`)
     } else {
-      console.log(`${record._id} saved.`)
+      console.log(`Record ${record._id} saved.`)
+      res.send(JSON.stringify({hash: record._id}))
     }
   })
-  res.send(record._id)
 })
 
-app.get('/:hash', function (req, res) {
-  console.log(`Looking record up by ${req.params.hash}`)
+app.get('/locations/:hash', function (req, res) {
+  let hash = req.params.hash
+  console.log(`Looking record up by ${hash}`)
   Location.findOne({
     '_id': req.params.hash
   }, function (err, location) {
     if (err) {
-      console.log(`Error when retrieving ${req.params.hash}: ${err}`)
+      console.log(`Error when retrieving ${hash}: ${err}`)
     } else {
+      console.log(`${hash} found.`)
       res.send(location)
     }
   })
@@ -53,3 +45,25 @@ app.get('/:hash', function (req, res) {
 app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'))
 })
+
+function connectToMongo() {
+  console.log('attempting to connect')
+  mongoose.connect('mongodb://waypoints:waypoints@ds021671.mlab.com:21671/heroku_27wrdm9m')
+
+  let db = mongoose.connection
+  db.on('error', console.error.bind(console, 'connection error:'))
+  db.once('open', function() {
+    console.log('connected to db')
+    Location = mongoose.model('locations', {
+      name: String,
+      geo: {
+        lat: Number,
+        long: Number
+      },
+      _id: {
+        type: String,
+        'default': shortid.generate
+      }
+    })
+  })
+}
--- public/app.js
+++ public/app.js
@@ -1,39 +1,52 @@
-var map;
-var currentLocation = {
-  lat: 45.4,
-  lng: -122.7
-};
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 8,
-    center: currentLocation
-  });
-
-  navigator.geolocation.getCurrentPosition(function (position) {
-    var pos = {
-      lat: position.coords.latitude,
-      lng: position.coords.longitude
-    };
-
-    map.setCenter(pos);
+function showSection(toShow) {
+  ['search', 'map', 'hash'].forEach(function(section) {
+    $('#' + section).toggle(section === toShow)
   })
+}
 
-  var input = document.getElementById('search');
-
-  var autocomplete = new google.maps.places.Autocomplete(input);
+function init() {
+  showSection('search')
+  var autocomplete = new google.maps.places.Autocomplete($('#search input')[0])
 
   autocomplete.addListener('place_changed', function () {
-    var place = autocomplete.getPlace();
+    var place = autocomplete.getPlace()
+
+    $('#map input').val(place.name);
+    showSection('map')
+    var map = new google.maps.Map($('#map div')[0])
+
+    var location = {
+      name: place.name,
+      geo: {
+        lat: place.geometry.location.lat(),
+        long: place.geometry.location.lng()
+      }
+    }
+
+    $('#map button').click(function () {
+      console.log(`Posting location: ${JSON.stringify(location)}`)
+
+      $.ajax({
+        url: '/locations',
+        type: 'POST',
+        data: JSON.stringify(location),
+        contentType: 'application/json; charset=utf-8',
+        success: function (data) {
+          console.log(`POST successful. Response: ${JSON.stringify(data)}`)
+          $('#hash span').text(data.hash)
+          showSection('hash')
+        }
+      })
+    })
 
     if (place.geometry.viewport) {
-      map.fitBounds(place.geometry.viewport);
+      map.fitBounds(place.geometry.viewport)
     } else {
       var marker = new google.maps.Marker({
         map: map,
         position: place.geometry.location
-      });
-      map.setZoom(16)
+      })
+      map.setZoom(14)
       map.setCenter(place.geometry.location)
     }
   })
