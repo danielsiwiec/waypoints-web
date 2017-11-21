e355e5369f3681c7456f1ca2164e806098527f5d
Dan Siwiec <daniel.siwiec@gmail.com>
2017-11-20 21:43:49 -0800

    Refresh npm dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -15,11 +15,11 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
+        loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
       },
       {
         test: /.json$/,
-        loader: 'json'
+        loader: 'json-loader'
       }
     ]
   },
