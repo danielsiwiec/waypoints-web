4cd0c8f82a74af50895b05563e01645549c1a46d
Dan Siwiec <daniel.siwiec@gmail.com>
2022-11-28 20:18:31 -0800

    Add schema validation

--- server/api/post.js
+++ server/api/post.js
@@ -1,7 +1,37 @@
 import analytics from '../analytics'
+import Ajv from 'ajv'
 
 module.exports = Location => {
+
+  const ajv = new Ajv()
+
+  const schema = {
+    type: 'object',
+    properties: {
+      name: {type: 'string'},
+      geo: {
+        type: 'object',
+        properties: {
+          lat: {type: 'number'},
+          lng: {type: 'number'}
+        },
+        required: ['lat', 'lng']
+      },
+    },
+    required: ['name', 'geo'],
+    additionalProperties: false,
+  }
+
+  const validate = ajv.compile(schema)
+
   return async (req, res) => {
+
+    if (!validate(req.body)) {
+      console.log(validate.errors)
+      res.sendStatus(400)
+      return
+    }
+
     let location = convertLongToLng(req)
     console.log(`Saving record ${JSON.stringify(req.body)}`)
     analytics.log('save', req.query.source)
